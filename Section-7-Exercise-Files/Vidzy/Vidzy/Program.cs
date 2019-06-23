a7ac414b0cd975199f6bdd623f5d70ae6ad630b9
alexlezama90 <alexlezama90@hotmail.com>
2019-06-22 23:52:56 -0500

    Exercise 7 Finished

---
+++ Section-7-Exercise-Files/Vidzy/Vidzy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyContext();
+            //var videos = context.Videos; //Lazy loading
+            //var videos = context.Videos.Include(c=>c.Genre).ToList();
+            var videos = context.Videos.ToList(); //Explicit (1)
+            //var videoGenreIds = videos.Select(v => v.GenreId); //Explicit (1.5)
+            context.Genres.Load(); //*Opcion de Mosh
+            //context.Genres.Where(g => videoGenreIds.Contains(g.Id)).Load(); //Explicit (2) *DO NOT FORGET THE LOAD!!!!
+            foreach (var video in videos)
+                Console.WriteLine("{0}, Genre: {1}", video.Name, video.Genre.Name);
+        }
+    }
+}
